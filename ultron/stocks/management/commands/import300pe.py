9095318f786fc66d3e368e6e0f36cc7022402771
Xiaoli Wang <dot@Xiaolis-iMac.local>
2021-06-28 19:37:19 +0800

    import300pe

---
+++ ultron/stocks/management/commands/import300pe.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+from datetime import date
+import json
+import requests
+
+from django.core.management.base import BaseCommand, CommandError
+from stocks.models import Stock, KHistory
+
+import csv
+
+class Command(BaseCommand):
+    help = '导入沪深300指数的PE-TTM数据'
+    
+    def handle(self, *args, **options):
+        
+        with open('300.csv')as f:
+            f_csv = csv.reader(f)
+            for row in f_csv:
+                if len(row) > 0:
+                    print(row[0], row[4])
+                    khs = KHistory.objects.filter(code="sh.000300", date=row[0])
+                    for k in khs:
+                        k.peTTM = str(row[4])
+                        k.save()            
