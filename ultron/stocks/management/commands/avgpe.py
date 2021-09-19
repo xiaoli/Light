dcf2b07d498f63a608435a45793de8dad9e941a6
Xiaoli Wang <dot@Xiaolis-iMac.local>
2021-09-19 19:17:43 +0800

    计算PE时增加异常处理

--- ultron/stocks/management/commands/avgpe.py
+++ ultron/stocks/management/commands/avgpe.py
@@ -35,40 +35,43 @@ class Command(BaseCommand):
                 json_value_list = {}
                 # 计算 1-10 年
                 for i in range(1, 11):
-                    # 当前年份下的数据
-                    current_year = {}
+                    try:
+                        # 当前年份下的数据
+                        current_year = {}
                     
-                    start_date = end_date.replace( year = end_date.year - i )
+                        start_date = end_date.replace( year = end_date.year - i )
 
-                    all_pe_set = KHistory.objects.filter(date__gte=start_date, date__lt=end_date, stock=s).exclude(peTTM=0)
+                        all_pe_set = KHistory.objects.filter(date__gte=start_date, date__lt=end_date, stock=s).exclude(peTTM=0)
                     
-                    # MM策略所需
-                    pe = all_pe_set.aggregate(Avg('peTTM'), Max('peTTM'), Min('peTTM'))
-                    current_year["maxPE"] = pe.get('peTTM__max')
-                    current_year["minPE"] = pe.get('peTTM__min')
-                    current_year["avgPE"] = pe.get('peTTM__avg')
+                        # MM策略所需
+                        pe = all_pe_set.aggregate(Avg('peTTM'), Max('peTTM'), Min('peTTM'))
+                        current_year["maxPE"] = pe.get('peTTM__max')
+                        current_year["minPE"] = pe.get('peTTM__min')
+                        current_year["avgPE"] = pe.get('peTTM__avg')
                 
-                    # 均值策略所需
-                    percent_count = int(all_pe_set.count() * 0.3)
-                    h_pe = all_pe_set.order_by('-peTTM')[:percent_count].aggregate(Sum('peTTM'))
-                    l_pe = all_pe_set.order_by('peTTM')[:percent_count].aggregate(Sum('peTTM'))
-                    current_year["avgMaxPE"] = h_pe.get('peTTM__sum') / percent_count
-                    current_year["avgMinPE"] = l_pe.get('peTTM__sum') / percent_count
+                        # 均值策略所需
+                        percent_count = int(all_pe_set.count() * 0.3)
+                        h_pe = all_pe_set.order_by('-peTTM')[:percent_count].aggregate(Sum('peTTM'))
+                        l_pe = all_pe_set.order_by('peTTM')[:percent_count].aggregate(Sum('peTTM'))
+                        current_year["avgMaxPE"] = h_pe.get('peTTM__sum') / percent_count
+                        current_year["avgMinPE"] = l_pe.get('peTTM__sum') / percent_count
                 
-                    # 均值策略v1版所需
-                    current_year["h_pe_list"] = []
-                    current_year["l_pe_list"] = []
-                    for x in range(0, 30):
-                        h_pe_list = all_pe_set.order_by('-peTTM')[int(all_pe_set.count() * x * 0.01):int(all_pe_set.count() * (x+1) * 0.01)]
-                        h_pe = h_pe_list.aggregate(Sum('peTTM'))
-                        #print(h_pe_list.values_list('peTTM', flat = True), len(h_pe_list), int(all_pe_set.count() * 0.01), h_pe.get('peTTM__sum') / len(h_pe_list))
-                        current_year["h_pe_list"].append( h_pe.get('peTTM__sum') / len(h_pe_list) )
-                        l_pe_list = all_pe_set.order_by('peTTM')[int(all_pe_set.count() * x * 0.01):int(all_pe_set.count() * (x+1) * 0.01)]
-                        l_pe = l_pe_list.aggregate(Sum('peTTM'))
-                        current_year["l_pe_list"].append( l_pe.get('peTTM__sum') / len(h_pe_list) )
-                        #print(l_pe_list.values_list('peTTM', flat = True), len(l_pe_list), int(all_pe_set.count() * 0.01), l_pe.get('peTTM__sum') / len(l_pe_list))
+                        # 均值策略v1版所需
+                        current_year["h_pe_list"] = []
+                        current_year["l_pe_list"] = []
+                        for x in range(0, 30):
+                            h_pe_list = all_pe_set.order_by('-peTTM')[int(all_pe_set.count() * x * 0.01):int(all_pe_set.count() * (x+1) * 0.01)]
+                            h_pe = h_pe_list.aggregate(Sum('peTTM'))
+                            #print(h_pe_list.values_list('peTTM', flat = True), len(h_pe_list), int(all_pe_set.count() * 0.01), h_pe.get('peTTM__sum') / len(h_pe_list))
+                            current_year["h_pe_list"].append( h_pe.get('peTTM__sum') / len(h_pe_list) )
+                            l_pe_list = all_pe_set.order_by('peTTM')[int(all_pe_set.count() * x * 0.01):int(all_pe_set.count() * (x+1) * 0.01)]
+                            l_pe = l_pe_list.aggregate(Sum('peTTM'))
+                            current_year["l_pe_list"].append( l_pe.get('peTTM__sum') / len(h_pe_list) )
+                            #print(l_pe_list.values_list('peTTM', flat = True), len(l_pe_list), int(all_pe_set.count() * 0.01), l_pe.get('peTTM__sum') / len(l_pe_list))
                     
-                    json_value_list["Y%s" % i] = current_year
+                        json_value_list["Y%s" % i] = current_year
+                    except Exception as e:
+                        pass
                 
                 h.metrics_value = json.dumps(json_value_list, use_decimal=True)
                 #print(h.metrics_value)
