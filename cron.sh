c119f0d0fade40d8cce20b79890155386995f02c
Xiaoli Wang <dot@Xiaolis-iMac.local>
2021-10-15 16:17:02 +0800

    update cron.sh

--- cron.sh
+++ cron.sh
@@ -6,7 +6,7 @@ python manage.py updatestocks --settings=ultron.settings.prod
 python manage.py updatehistory --settings=ultron.settings.prod
 #股票和指数分开之后，股票在baostock上默认就有pe，而指数在理性人上也默认就有pe，不再需要单独update pe了
 #python manage.py updatehistorype --settings=ultron.settings.prod
-python manage.py avgpe --settings=ultron.settings.prod
+#python manage.py avgpe --settings=ultron.settings.prod
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "$dt" >> /srv/www/test.log
